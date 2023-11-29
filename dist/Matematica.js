ec01d173b3f10690ef9455d25c6013bb726a795a
sdlima4025 <sergiolimafg3@gmail.com>
2023-11-29 18:22:08 -0300

    criado projeto de estudo NODEjs

---
+++ dist/Matematica.js
@@ -0,0 +1,20 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+function somar(x, y) {
+    return x + y;
+}
+function subtrair(x, y) {
+    return x - y;
+}
+function multiplicar(x, y) {
+    return x * y;
+}
+function dividir(x, y) {
+    return x / y;
+}
+exports.default = {
+    somar,
+    subtrair,
+    multiplicar,
+    dividir,
+};
---
+++ src/index.ts
@@ -0,0 +1,4 @@
+import validator from 'validator';
+
+let ip = '127.0.0.1';
+console.log(validator.isIP(ip));
